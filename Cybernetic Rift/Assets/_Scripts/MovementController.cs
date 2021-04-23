0196e1bcd67d5f1e17d57229b7a9971837bbb196
Jeffrey Freet <lethal.injection.studio@gmail.com>
2021-04-23 13:52:07 -0400

    Base Player Controller Added

--- Cybernetic Rift/Assets/_Scripts/MovementController.cs
+++ Cybernetic Rift/Assets/_Scripts/MovementController.cs
@@ -7,14 +7,15 @@ public class MovementController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private Camera _camera;
-    private Vector3 _movement;
-    private Vector3 _movementDirection;
+    [SerializeField] private Vector3 _movement;
+    [SerializeField] Vector3 _rotation;
+    [SerializeField] private Vector3 _movementDirection;
     private bool _jumped = false;
     private float _turnSmoothTime = 0.1f;
     private float _turnSmoothVelocity;
 
 
-    [SerializeField] private float _movementSpeed = 3f;
+    [SerializeField] private float _movementSpeed = 15f;
     [SerializeField] private float _jumpForce = 5f;
 
     void Awake() 
@@ -32,6 +33,7 @@ public class MovementController : MonoBehaviour
     void Update()
     {
         _movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        _rotation = new Vector3(0, Input.GetAxisRaw("Mouse X"), 0);
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
@@ -46,11 +48,11 @@ public class MovementController : MonoBehaviour
             float _targetAngle = Mathf.Atan2(_movement.x, _movement.z) * Mathf.Rad2Deg + _camera.transform.eulerAngles.y;
             float _angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, _angle, 0f);
-
+            //_rigidbody.rotation = Quaternion.Euler(0f, _angle, 0f);
             _movementDirection = Quaternion.Euler(0f, _targetAngle, 0f) * Vector3.forward * _movementSpeed;
             transform.position += _movementDirection * Time.fixedDeltaTime * _movementSpeed;
+            //_rigidbody.velocity = _movementDirection * Time.fixedDeltaTime * _movementSpeed;
         }
-
 
         if (_jumped)
         {
@@ -59,7 +61,6 @@ public class MovementController : MonoBehaviour
         }
     }
 }
-
 
 
   
