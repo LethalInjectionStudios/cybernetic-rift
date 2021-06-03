8574b0c65138091711022d3cb0afe0f127065d02
Lethal Injection Stuios <lethal.injecion.studio@gmail.com>
2021-06-03 10:22:24 -0400

    Added Quest Log

--- Cybernetic Rift/Assets/_Scripts/DevTools/DevTools.cs
+++ Cybernetic Rift/Assets/_Scripts/DevTools/DevTools.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DevTools : MonoBehaviour
 {
     public List<BaseItem> itemDB = new List<BaseItem>();
+    public List<Quest> questDB = new List<Quest>();
     public Pickup pickupPrefab;
 
     // Update is called once per frame
@@ -14,8 +15,13 @@ public class DevTools : MonoBehaviour
         {
             Pickup pickup = Instantiate(pickupPrefab);
             pickup.transform.position = new Vector3(0,0.5f,0);
-            int rand = Random.Range(0,2);
+            int rand = Random.Range(0,itemDB.Count);
             pickup.LoadData(itemDB[rand]);
+        }
+
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            GetComponent<QuestLog>().AddQuest(questDB[Random.Range(0, questDB.Count)]);
         }
     }
 }
--- Cybernetic Rift/Assets/_Scripts/Player/QuestLog.cs
+++ Cybernetic Rift/Assets/_Scripts/Player/QuestLog.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class QuestLog : MonoBehaviour
 {
-    private List<Quest> _questList;
+    [SerializeField] private List<Quest> _questList;
 
     public void AddQuest(Quest quest)
     {
-
+        _questList.Add(quest);
     }
 
-    public void CompleteQuest()
+    public void CompleteQuest(Quest quest)
     {
-
+        foreach(Quest _quest in _questList )
+        {
+            if(_quest == quest)
+            {
+                _quest.isCompleted = true;
+            }
+        }
     }
 
     public List<Quest> GetQuestList()
