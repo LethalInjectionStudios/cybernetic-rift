6a62972ee9e82fb825a844bdd1236693230f7b42
Jeffrey Freet <lethal.injection.studio@gmail.com>
2021-06-02 22:01:37 -0400

    EoD push

---
+++ Cybernetic Rift/Assets/_Scripts/Player/QuestLog.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestLog : MonoBehaviour
+{
+    private List<Quest> _questList;
+
+    public void AddQuest(Quest quest)
+    {
+
+    }
+
+    public void CompleteQuest()
+    {
+
+    }
+
+    public List<Quest> GetQuestList()
+    {
+        return _questList;
+    }
+}
